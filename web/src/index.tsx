a4bf97ef343408843a55bb9a904bc7f829cdbe96
darryl <darrylnatale@gmail.com>
2023-02-01 13:19:25 -0500

    Insert Context Provider

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import { reportWebVitals } from './reportWebVitals';
-
+import Provider from './Context';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
