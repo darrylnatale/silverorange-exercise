acb998a4c12e5fe840d3b343a55a284013940f66
darryl <darrylnatale@gmail.com>
2023-02-01 14:01:22 -0500

    Move repo detail fetches to parent

--- web/src/components/List.js
+++ web/src/components/List.js
@@ -1,9 +1,12 @@
 import Repo from './Repo';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { Context } from '../Context';
+import ReactMarkdown from 'react-markdown';
 
 const List = ({ repos }) => {
   const [filtered, setFiltered] = useState(repos);
-
+  const [selectedRepo, setSelectedRepo] = useState(null);
+  const { commit, setCommit, readMe, setReadMe } = useContext(Context);
   repos.sort((a, b) => {
     return new Date(b.created_at) - new Date(a.created_at);
   });
@@ -16,6 +19,31 @@ const List = ({ repos }) => {
     setFiltered(repos.filter((repo) => repo.language === language));
   };
 
+  const handleRepoClick = (repo) => {
+    setSelectedRepo(repo);
+
+    fetch(`${repo.commits_url.replace('{/sha}', '')}`)
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        setCommit({ data: data[0].commit });
+      });
+
+    fetch(
+      `https://raw.githubusercontent.com/${repo.full_name}/master/README.md`
+    )
+      .then((res) => {
+        if (res.status === 200) {
+          return res.text();
+        }
+      })
+      .then((data) => {
+        setReadMe(data);
+      });
+  };
   return (
     <>
       <button onClick={() => setFiltered(repos)}>Show All</button>
@@ -27,22 +55,22 @@ const List = ({ repos }) => {
         );
       })}
 
-      {filtered ? (
+      {filtered &&
         filtered.map((repo) => {
-          return (
-            <Repo
-              key={repo.id}
-              name={repo.name}
-              description={repo.description}
-              forksCount={repo.forks_count}
-              language={repo.language}
-              fullName={repo.full_name}
-              commitsUrl={repo.commits_url}
-            />
-          );
-        })
-      ) : (
-        <>hi</>
+          return <Repo key={repo.id} repo={repo} onClick={handleRepoClick} />;
+        })}
+      {selectedRepo && <div>selected repo</div>}
+      {commit && (
+        <>
+          <div>{commit.data.author.name}</div>
+          <div>{commit.data.author.date}</div>
+          <div>{commit.data.message}</div>
+        </>
+      )}
+      {readMe && (
+        <>
+          <ReactMarkdown>{readMe}</ReactMarkdown>
+        </>
       )}
     </>
   );
--- web/src/components/Repo.js
+++ web/src/components/Repo.js
@@ -1,63 +1,12 @@
-import { useState } from 'react';
-
-const Repo = ({
-  name,
-  description,
-  forksCount,
-  language,
-  fullName,
-  commitsUrl,
-}) => {
-  const [commit, setCommit] = useState();
-  const [readMe, setReadMe] = useState();
-
-  const handleClick = (url, full_name) => {
-    if (commit) {
-      setCommit();
-      setReadMe();
-    } else {
-      fetch(`${url.replace('{/sha}', '')}`)
-        .then((res) => {
-          if (res.status === 200) {
-            return res.json();
-          }
-        })
-        .then((data) => {
-          setCommit(data[0].commit);
-        });
-
-      fetch(`https://raw.githubusercontent.com/${full_name}/master/README.md`)
-        .then((res) => {
-          if (res.status === 200) {
-            return res.text();
-          }
-        })
-        .then((data) => {
-          setReadMe(data);
-        });
-    }
-  };
-
+const Repo = ({ repo, fullName, commitsUrl, onClick }) => {
   return (
     <div>
-      <button onClick={() => handleClick(commitsUrl, fullName)}>
-        <span>{name}</span>
-        <span>{description}</span>
-        <span>{forksCount} forks</span>
-        <span>Language: {language}</span>
+      <button onClick={() => onClick(repo)}>
+        <span>{repo.name}</span>
+        <span>{repo.description}</span>
+        <span>{repo.forksCount} forks</span>
+        <span>Language: {repo.language}</span>
       </button>
-      {commit && (
-        <>
-          <div>{commit.author.name}</div>
-          <div>{commit.author.date}</div>
-          <div>{commit.message}</div>
-        </>
-      )}
-      {readMe && (
-        <>
-          <div>{readMe}</div>
-        </>
-      )}
     </div>
   );
 };
